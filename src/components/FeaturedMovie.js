50d117dbdf0a3d3234ca4033296a5e5968c02888
ewbriao1978 <eduardo.briao@gmail.com>
2022-04-11 00:16:09 -0300

    Update all files

---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import './FeaturedMovie.css';
+
+export default ({item}) => {
+
+    let firstDate = new Date(item.first_air_date);
+
+    return (
+
+        <section className="featured" style={{
+            
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: 'center',
+            backgroundImage: `url(http://image.tmdb.org/t/p/original${item.backdrop_path})`
+    
+        }}  >
+            
+
+            <div className = "featured--vertical">
+
+                <div className = "featured--horizontal">
+
+                    <div className = "featured--name">{item.original_name}</div>
+                    <div className = "featured--info">
+                        <div className = "featured--points">{item.vote_average} pontos</div>
+                        <div className = "featured--year">{firstDate.getFullYear()}</div>
+                        <div className = "featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons!==1 ? 's':''}</div>
+                        <div className = "featured--description">{item.overview}</div>
+                        <div className = "featured--button">
+                        </div>
+
+                        <div className="featured--genred"><strong>Gêneros:</strong> 
+
+                        
+                        </div>
+
+
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            
+
+
+
+
+        </section>
+
+        
+    );
+}
